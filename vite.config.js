ad1caa7d24419791d4e8347dc0a17b03a84cb184
Obed Boateng <89081871+RealOkab@users.noreply.github.com>
2025-05-20 16:52:46 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: process.env.PORT || 10000,
-    allowedHosts: ["klinchem-enterprise-frontend.onrender.com"],
+    allowedHosts: ["klinchem-enterprise.onrender.com"],
   },
   base: "/klinchem/",
 });
