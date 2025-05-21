48816ec8346bba7b7c1181690ca8b2abf6f59393
Obed Boateng <okabonline@gmail.com>
2025-05-21 07:59:25 +0000

    ....

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,27 @@
-/* eslint-disable no-undef */
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
-// https://vite.dev/config/
-export default defineConfig({
-  mode: "production",
-  NODE_ENV: "production",
-  plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: process.env.PORT || 10000,
-    allowedHosts: ["klinchem-enterprise.onrender.com"],
-  },
-  base: "/klinchem/signIn",
-  build: {
-    minify: "terser",
-    rollupOptions: {
-      treeshake: true,
+import terser from "@rollup/plugin-terser";
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), ""); // Load environment variables
+
+  return {
+    mode: "production",
+    define: {
+      "process.env.NODE_ENV": JSON.stringify(env.NODE_ENV || "production"),
     },
-  },
+    plugins: [react(), terser()],
+    server: {
+      host: "0.0.0.0",
+      port: env.VITE_PORT || 10000, // Use `VITE_PORT` instead of `process.env.PORT`
+      allowedHosts: ["klinchem-enterprise.onrender.com"],
+    },
+    base: "/klinchem/signIn",
+    build: {
+      minify: "terser",
+      rollupOptions: {
+        treeshake: true,
+        plugins: [terser()],
+      },
+    },
+  };
 });
